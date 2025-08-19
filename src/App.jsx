aea0f64cdfbe572ccad136a3bd97b74746a00049
hym-dot <yumin6350@kbu.ac.kr>
2025-08-19 17:32:51 +0900

    jh

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,48 @@
+
 import './App.css'
-import { useReducer, useRef } from 'react'
-import { Routes, Route, useNavigate } from 'react-router-dom'
+import { useReducer, useRef, createContext,  useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Diary from './pages/Diary'
 import Edit from './pages/Edit'
 import Home from './pages/Home'
-import New from './pages/New'
 import Notfound from './pages/Notfound'
-import { getEmotionImage } from './util/getEmotionImage'
-import Header from './components/Header'
-import Button from './components/Button'
+import New from './pages/New'
+
+
 
 const mockData = [
   {
     id: 1,
-    createDate: new Date().getTime(),
+    createdDate: new Date("2025-08-17").getTime(),
     emotionId: 1,
     content: "1번 일기 내용"
   },
   {
     id: 2,
-    createDate: new Date().getTime(),
+    createdDate: new Date("2025-07-05").getTime(),
     emotionId: 2,
     content: "2번 일기 내용"
+  },
+  {
+    id: 3,
+    createdDate: new Date("2024-12-05").getTime(),
+    emotionId: 4,
+    content: "3번 일기 내용"
   }
 ]
 
 function reducer(state, action) {
   switch (action.type) {
+    case "INIT":
+      return action.data
     case "CREATE":
       return [action.data, ...state]
-
     case "UPDATE":
       return state.map((item) =>
         String(item.id) === String(action.data.id) ?
           action.data
           : item
       )
-
     case "DELETE":
       return state.filter(
         (item) => String(item.id) !== String(action.id)
@@ -44,37 +50,46 @@ function reducer(state, action) {
     default:
       return state
   }
+
 }
 
+export const DiaryStateContext = createContext()
+export const DiaryDispatchContext = createContext()
 function App() {
 
   const [data, dispatch] = useReducer(reducer, mockData)
-  const idRef = useRef(3)
+  const idRef = useRef(4)
 
-  const onCreate = (createDate, emotionId, content) => {
+  useEffect(()=>{
+    dispatch({
+      type:"INIT",
+      data:mockData
+    })
+  },[])
+
+  const onCreate = (createdDate, emotionId, content) => {
 
     dispatch({
       type: "CREATE",
       data: {
         id: idRef.current++,
-        createDate,
+        createdDate,
         emotionId,
         content
       }
     })
   }
-
-  const onUpdate = (id, createDate, emotionId, content) => {
-  dispatch({
-    type: "UPDATE",
-    data: {
-      id,
-      createDate,
-      emotionId,
-      content,
-    }
-  })
-}
+  const onUpdate = (id, createdDate, emotionId, content) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id,
+        createdDate,
+        emotionId,
+        content
+      }
+    })
+  }
 
   const onDelete = (id) => {
     dispatch({
@@ -82,36 +97,18 @@ function App() {
       id
     })
   }
-
   return (
-    <div>
-      <Header
-        leftChild={<Button text="left" type="POSITIVE" />}
-        title="header title"
-        rightChild={<Button text="right" type="NEGATIVE" />}
-      />
-
-      <button onClick={() => onCreate(
-        new Date().getTime(), 1, "hello"
-      )}>일기 추가하기</button>
-
-      <button onClick={() => onUpdate(
-        6, new Date().getTime(), 3, "수정 된 내용"
-      )}>일기 수정하기</button>
-
-      <button onClick={() => onDelete(5)}>일기 삭제하기</button>
-
-
-
-
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/new' element={<New />} />
-        <Route path='/edit/:id' element={<Edit />} />
-        <Route path='/diary/:id' element={<Diary />} />
-        <Route path='*' element={<Notfound />} />
-      </Routes>
-    </div>
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider value={{onCreate,onUpdate,onDelete}}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/new' element={<New />} />
+          <Route path='/edit/:id' element={<Edit />} />
+          <Route path='/diary/:id' element={<Diary />} />
+          <Route path='*' element={<Notfound />} />
+        </Routes>
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import Header from '../components/Header'
+import Button from '../components/Button'
+import DiaryList from '../components/DiaryList'
+import { DiaryStateContext } from '../App'
 
 const Home = () => {
+  const data = useContext(DiaryStateContext)
+
+  const [pivoteDate, setPivotDate] = useState(new Date())
+
+  const getMonthlyData = (pivoteDate, data) => {
+    const beginTime = new Date(
+      pivoteDate.getFullYear(),
+      pivoteDate.getMonth(),
+      1,
+      0, 0, 0
+    ).getTime()
+
+    const endTime = new Date(
+      pivoteDate.getFullYear(),
+      pivoteDate.getMonth(),
+      1,
+      23, 59, 59
+    ).getTime()
+
+    return data.filter(
+      (item)=>beginTime <=item.createdDate && item.createdDate<=endTime
+    )
+  }
+
+  const monthlyData = getMonthlyData(pivoteDate,data)
+
+  const onIncreamentMonth = () => {
+    setPivotDate(
+      new Date(pivoteDate.getFullYear(), pivoteDate.getMonth() + 1)
+    )
+  }
+
+  const onDecreamentMonth = () => {
+    setPivotDate(
+      new Date(pivoteDate.getFullYear(), pivoteDate.getMonth() - 1)
+    )
+  }
+
+
   return (
-    <div>Home</div>
+    <div>
+      <Header
+        leftChild={<Button text={"<"} onClick={onDecreamentMonth} />}
+        title={`${pivoteDate.getFullYear()}년   ${pivoteDate.getMonth() + 1}월`}
+        rightChild={<Button text={">"} onClick={onIncreamentMonth} />}
+      />
+      <DiaryList data={monthlyData} />
+    </div>
   )
 }
 
